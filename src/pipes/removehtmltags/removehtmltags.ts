4f07fd8c040a56d893085a7e684756a967605acc
Sanjay Kushwah <sanjay.kushwah@galaxyweblinks.in>
2018-07-19 13:20:28 +0530

    Remove html content from the input field

--- src/pipes/removehtmltags/removehtmltags.ts
+++ src/pipes/removehtmltags/removehtmltags.ts
@@ -15,14 +15,9 @@ export class RemovehtmltagsPipe implements PipeTransform {
     if (value) {
       var result = value.replace(/<\/?[^>]+>/gi, ""); //removing html tag using regex pattern
       result = result.replace(/&nbsp;/gi, " ") // removing Non-Breaking SPace (NBSP)
-      // result = this.replaceNbsps(result)
       return result;
     }
     else { }
   }
-
-  replaceNbsps(str) {
-    var re = new RegExp(String.fromCharCode(160), "g");
-    return str.replace(re, " ");
-  }
+  
 }
